41246b33274a22a2402f40f1717abe15e8346edc
HM <95107265+hafmohamga@users.noreply.github.com>
2024-01-25 20:48:56 -0500

    Update copy_files_in_adls_from_one_container_to_another.ps1

--- copy_files_in_adls_from_one_container_to_another.ps1
+++ copy_files_in_adls_from_one_container_to_another.ps1
@@ -23,3 +23,24 @@ $destContext = Get-AzStorageAccount -ResourceGroupName $destRG -Name $destAccoun
 # Copy folder recursively 
 Get-AzDataLakeGen2ChildItem -Context $sourceContext -FileSystem $sourceContainer -Path $sourcePath | 
     Copy-AzDataLakeGen2Item -Context $destContext -DestFileSystem $destContainer -DestPath $destPath -Recurse
+
+##########################################################################################################################################
+
+# Set source variables
+$sourceAccountName = "<source-account-name>"  
+$sourceContainer = "<source-container>"
+$sourceSAS= New-AzStorageContainerSASToken -Name $sourceContainer -Context $sourceCtx -Permission r -ExpiryTime (Get-Date).AddHours(2)
+$sourcePath = "<path-to-copy-from>"
+
+# Set destination variables
+$destAccountName = "<dest-account-name>" 
+$destContainer = "<dest-container>"
+$destSAS= New-AzStorageContainerSASToken -Name $destContainer -Permission rw -ExpiryTime (Get-Date).AddHours(2) -Context $destCtx
+$destPath = "<path-to-copy-to>"
+
+# Construct source and dest URIs
+$sourceUri = $sourceAccountName + ".dfs.core.windows.net/" + $sourceContainer + $sourceSAS
+$destUri = $destAccountName + ".dfs.core.windows.net/" + $destContainer + $destSAS
+
+# Perform copy
+azcopy copy $sourceUri/$sourcePath $destUri/$destPath --recursive=true
